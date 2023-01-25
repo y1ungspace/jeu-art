0631fc5761c53c847a7b818ba6fc843e537f4978
y1ungspace <y1ung.space@gmail.com>
2023-01-25 21:20:18 +0200

    rewrite header on react

--- my-app/src/header.tsx
+++ my-app/src/header.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
 
+import cartIcon from './assets/icons/shopping-cart 1.svg';
+import personIcon from './assets/icons/Icon color-1.svg';
+import lupeIcon from './assets/icons/Icon color.svg';
+
 export function Header() {
   return ( 
     <section className="home_header header">
@@ -12,10 +16,11 @@ export function Header() {
           <h5 className="header_link"><a>Blog</a></h5>
           <h5 className="header_link"><a>Our Story</a></h5>
         </div>
+        {/* <img src={cartIcon} alt="Logo" /> */}
         <div className="header_image-links">
-          <a className="header_icon" style = {{backgroundImage: "url('./assets/Icon color.svg')"}} onClick = {() => (document.querySelector('.filter_search') as HTMLInputElement).focus()}></a>
-          <a className="header_icon" style= {{backgroundImage: "url('./assets/shopping-cart 1.svg')"}}></a>
-          <a className="header_icon" style= {{backgroundImage: "url('./assets/Icon color-1.svg')"}}></a>
+          <img className="header_icon" src={lupeIcon} alt="Logo" onClick = {() => (document.querySelector('.filter_search') as HTMLInputElement).focus()} />
+          <img className="header_icon" src={cartIcon} alt="Logo" />
+          <img className="header_icon" src={personIcon} alt="Logo" />
         </div>
       </nav>
     </section>
